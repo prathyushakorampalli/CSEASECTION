6abfce312450595ad3a9688038d1993b448553b1
ITWS <ITWS@DESKTOP-SM7F9GB>
2023-09-21 14:02:21 +0530

    Team Lead commit

---
+++ CSEASECTION/src/com/lbrce/CSEASECTION/Sample.java
@@ -0,0 +1,15 @@
+package com.lbrce.CSEASECTION;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		prathyusha();
+	}
+	public static void prathyusha() {
+		// TODO Auto-generated method stub
+		System.out.println("This is Team Lead - PRATHYUSHA");
+	}
+
+
+}
